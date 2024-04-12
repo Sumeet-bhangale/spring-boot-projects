1b3261bb7177e059d5b74ba237afc155c16f4368
Sumeet-bhangale <92034732+Sumeet-bhangale@users.noreply.github.com>
2024-04-12 09:12:22 +0530

    Delete Cooking-Recipes-Application SpringBoot/Cooking Recipes SpringBoot directory

--- Cooking-Recipes-Application SpringBoot/Cooking Recipes SpringBoot/src/main/java/com/example/project/Constants/Messages.java
+++
@@ -1,6 +0,0 @@
-package com.example.project.Constants;
-
-public abstract class Messages {
-    public static String passwordAndConfirmPasswordNotMatched = "Password and Confirm password not matched";
-    public static String userAlreadyExists = "User already exists";
-}
--- Cooking-Recipes-Application SpringBoot/Cooking Recipes SpringBoot/src/main/java/com/example/project/Enums/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.example.project.Enums;
-
-public enum UserRole {
-    USER,
-    ADMIN
-}
